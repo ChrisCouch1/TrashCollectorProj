aa06bf16bb1956cf7b9bf340756ae33a0ee8232b
Christopher Couch <c.alan.couch@gmail.com>
2020-11-16 12:44:28 -0600

    Added member variables to Customer Model to capture Address, Trashday, SuspendTrashDate, a List of dates named SuspendedDates, OneTimePickUp, and Balance. Added migration and updated database. Created CRUD views for Customer with the new variables.

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -13,6 +13,15 @@ namespace TrashCollector.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public string TrashDay { get; set; }
+        public string SuspendTrashDate { get; set; }
+
+        public List<string> SuspendedDates;
+        public string OneTimePickUpDate { get; set; }
+
+        public double Balance { get; set; }
+
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
         public IdentityUser IdentityUser { get; set; }
